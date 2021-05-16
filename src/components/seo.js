5d9635b4bdb56976e70a3c5b527380989bb8d7be
Riley Bathurst <riley@rileybathurst.com>
2021-05-16 15:16:29 -0700

    seo fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,97 +6,89 @@ import { Helmet } from "react-helmet";
 import { useLocation } from "@reach/router";
 import { useStaticQuery, graphql } from "gatsby";
 
-const SEO = ({ title, description, pathname, article, image }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          siteUrl,
-          defaultDescription,
-          defaultImage,
-          ogImage,
-          twitterImage,
-          telephone,
-          openingHours,
-          faxNumber,
-          logo,
-          areaServed,
-          location,
-        },
-      },
+const SEO = ({ title, description, image, lang }) => {
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
+  const {
+    defaultTitle,
+    titleTemplate,
+    siteUrl,
+    defaultDescription,
+    defaultImage,
+    ogImage,
+    twitterImage,
+    telephone,
+    openingHours,
+    faxNumber,
+    logo,
+    areaServed,
+    location,
+  } = site.siteMetadata;
 
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: `${siteUrl}${image || defaultImage}`,
-        ogImage: ogImage,
-        twitterImage: twitterImage,
-        url: `${siteUrl}${pathname || "/"}`,
-        openingHours: openingHours,
-        telephone: telephone,
-        faxNumber: faxNumber,
-        logo: logo,
-        areaServed: areaServed,
-        streetAddress: location.address.streetAddress,
-        addressLocality: location.address.addressLocality,
-        postalCode: location.address.postalCode,
-      }
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    ogImage: ogImage,
+    twitterImage: twitterImage,
+    url: `${siteUrl}${pathname || "/"}`,
+    openingHours: openingHours,
+    telephone: telephone,
+    faxNumber: faxNumber,
+    logo: logo,
+    areaServed: areaServed,
+    streetAddress: location.address.streetAddress,
+    addressLocality: location.address.addressLocality,
+    postalCode: location.address.postalCode,
+  }
 
-      return (
-        <>
-          <Helmet
-            title={seo.title}
-            titleTemplate={titleTemplate}
-            htmlAttributes={{
-              lang: 'en-US',
-            }}
-          >
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.ogImage} />
-            <meta property="og:type" content="website" />
-            {seo.url && <meta property="og:url" content={seo.url} />}
-            {seo.title && <meta property="og:title" content={seo.title} />}
-            {seo.description && (
-              <meta property="og:description" content={seo.description} />
-            )}
-            {seo.image && <meta property="og:image" content={seo.ogImage} />}
-            {seo.image && <meta name="twitter:image" content={seo.twitterImage} />}
+  return (
+    <Helmet
+      title={seo.title}
+      titleTemplate={titleTemplate}
+      htmlAttributes={{
+        lang: 'en-US',
+      }}
+    >
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.ogImage} />
+      <meta property="og:type" content="website" />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.ogImage} />}
+      {seo.image && <meta name="twitter:image" content={seo.twitterImage} />}
 
-            {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
 
-            {seo.description && (
-              <meta name="twitter:description" content={seo.description} />
-            )}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
 
-            <meta property="og:image" content={seo.image} />
-            {seo.openingHours && (
-              <meta name="openingHours" content={seo.openingHours} />
-            )}
-            {seo.telephone && <meta name="telephone" content={seo.telephone} />}
-            {seo.faxNumber && <meta name="faxNumber" content={seo.faxNumber} />}
-            {seo.areaServed && <meta name="areaServed" content={seo.areaServed} />}
-            <meta name="logo" content={seo.logo} />
-            <meta
-              name="location"
-              content={
-                seo.streetAddress +
-                ", " +
-                seo.addressLocality +
-                ", " +
-                seo.postalCode
-              }
-            />
-            <html lang="en" />
-          </Helmet>
-        </>
-      )
-    }}
-  />
-)
+      <meta property="og:image" content={seo.image} />
+      {seo.openingHours && (
+        <meta name="openingHours" content={seo.openingHours} />
+      )}
+      {seo.telephone && <meta name="telephone" content={seo.telephone} />}
+      {seo.faxNumber && <meta name="faxNumber" content={seo.faxNumber} />}
+      {seo.areaServed && <meta name="areaServed" content={seo.areaServed} />}
+      <meta name="logo" content={seo.logo} />
+      <meta
+        name="location"
+        content={
+          seo.streetAddress +
+          ", " +
+          seo.addressLocality +
+          ", " +
+          seo.postalCode
+        }
+      />
+      <html lang="en" />
+    </Helmet>
+  );
+};
 
 export default SEO
 
@@ -152,7 +144,6 @@ const query = graphql`
           address {
             _type
             addressLocality
-            addressRegion
             postalCode
             streetAddress
           }
@@ -161,5 +152,3 @@ const query = graphql`
     }
   }
 `
-
-// titleTemplate // I pulled this out as it was breaking things but I should rebuild it
